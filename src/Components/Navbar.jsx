83bf48e59fe6bff53a7b99863a5e4d9406014120
MaNomba <31630620+RafihYahya@users.noreply.github.com>
2023-11-04 22:19:44 +0100

    Update Navbar.jsx
    small_fix

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -9,7 +9,7 @@ const [toggle, settoggle] = useState(false)
   return (
     <div id='navbar' className=' fixed z-50 w-screen sm:w-full h-[55px]'>
         <div className=' w-full h-full bg-[#1b1b1b] text-center shadow-lg  '>
-            <div className='  max-w-[1500px] w-full h-full mx-auto sm:flex items-center justify-between '>
+            <div className=' px-5 max-w-[1500px] w-full h-full mx-auto sm:flex items-center justify-between '>
                 <h1 className='sm:inline-block hidden font-bold  text-2xl text-red-500 '><span className='text-red-400'>Bio</span>Anime</h1>
                 <ul className='flex items-center  justify-center '>
                     <li className='font-semibold  text-gray-300  px-4 mt-2 pb-4 hover:border-b-4 border-red-400 hover:text-white hover:scale-105 transition ease-in-out duration-150'><a href="/">Anime</a></li>
